ef1e01fe134b9824b1835f101c0710f9dc193510
Trang <ngminhtrang1999@gmail.com>
2021-07-28 03:02:50 +0700

    Testing

--- Get_garena_divine_v1.py
+++ Get_garena_divine_v1.py
@@ -8,26 +8,31 @@ import datetime
 page_link = ['https://divineshop.vn/garena-520', 'https://divineshop.vn/garena-500', 'https://divineshop.vn/garena-2400']
 price = [520, 500, 1000]
 
-while True:
-    print(datetime.datetime.now())
-    for index, i in enumerate(page_link):
-        page_response = requests.get(i)
+print(datetime.datetime.now())
+for index, i in enumerate(page_link):
+	page_response = requests.get(i)
 
-        page_content = BeautifulSoup(page_response.content, "html.parser")
+	page_content = BeautifulSoup(page_response.content, "html.parser")
 
-        # print(page_content)
+	# print(page_content)
 
-        product_names = page_content.findAll("div", attrs={"class" : "text-wrap"})
-        try:
-            print((product_names[1].span.text))
-        except IndexError as err:
-            print("Page chua mo")
-            continue
-        # Còn hàng
-        if product_names[1].span.text == "Còn hàng":
-            text = "Có hàng " + str(price[index]) + " rồi đại vương ơiiiii"
-            lang = "vi"
-            speech = Speech(text, lang)
-            speech.play()
+	product_names = page_content.findAll("div", attrs={"class" : "text-wrap"})
+   
+	# Còn hàng
+	if product_names:
+		if product_names[1].span.text == "Còn hàng":
+			text = "Có hàng " + str(price[index]) + " rồi đại vương ơiiiii"
+			lang = "vi"
+			speech = Speech(text, lang)
+			speech.play()
+			
+# content of test_sample.py
+def inc(x):
+    return x + 1
 
-    time.sleep(60)
+
+def test_answer():
+    assert inc(3) == 4
+	
+def test_answer():
+    assert inc(5) == 6

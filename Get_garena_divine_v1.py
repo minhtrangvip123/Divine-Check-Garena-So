ba74b50b54f772e43be2573c8761fcd2da810c39
Trang <ngminhtrang1999@gmail.com>
2021-07-28 01:17:30 +0700

    Revert "Fix UnicodeEncodeError and No loop"
    This reverts commit 0920235a5b6458c8a05b834ea9194d8ae2f4a70f.

--- Get_garena_divine_v1.py
+++ Get_garena_divine_v1.py
@@ -8,22 +8,26 @@ import datetime
 page_link = ['https://divineshop.vn/garena-520', 'https://divineshop.vn/garena-500', 'https://divineshop.vn/garena-2400']
 price = [520, 500, 1000]
 
+while True:
+    print(datetime.datetime.now())
+    for index, i in enumerate(page_link):
+        page_response = requests.get(i)
 
-print(datetime.datetime.now())
-for index, i in enumerate(page_link):
-	page_response = requests.get(i)
+        page_content = BeautifulSoup(page_response.content, "html.parser")
 
-	page_content = BeautifulSoup(page_response.content, "html.parser")
+        # print(page_content)
 
-	# print(page_content)
+        product_names = page_content.findAll("div", attrs={"class" : "text-wrap"})
+        try:
+            print((product_names[1].span.text))
+        except IndexError as err:
+            print("Page chua mo")
+            continue
+        # Còn hàng
+        if product_names[1].span.text == "Còn hàng":
+            text = "Có hàng " + str(price[index]) + " rồi đại vương ơiiiii"
+            lang = "vi"
+            speech = Speech(text, lang)
+            speech.play()
 
-	product_names = page_content.findAll("div", attrs={"class" : "text-wrap"})
-	
-	# Còn hàng
-	if product_names:
-		if product_names[1].span.text == "Hết hàng":
-			text = "Có hàng " + str(price[index]) + " rồi đại vương ơiiiii"
-			lang = "vi"
-			speech = Speech(text, lang)
-			speech.play()
-
+    time.sleep(60)
